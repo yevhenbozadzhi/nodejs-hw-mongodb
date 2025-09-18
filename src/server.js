7efd51585d07675919a7cce42f2f0b73f13d61b0
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-09-19 00:08:26 +0300

    added status&mes

--- src/server.js
+++ src/server.js
@@ -26,6 +26,8 @@ export const setupServer = () => {
     app.get('/contacts', async (req, res) => {
         const contacts = await getAllContacts();
         res.status(200).json({
+            status: 200,
+            message: 'Successfully found contacts!',
             data: contacts,
         });
     });
