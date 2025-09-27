d55961896c4be1368ad0cdb5f6d741fff89025a5
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-09-28 01:26:29 +0300

    added patch

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { createContactController, deleteContactController, getContactsController, getContactsIdController, patchContactController } from "../controllers/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
 import { isValidId } from "../middlewares/isValidId.js";
-import { createContactsSchema } from "../validation/contacts.js";
+import { createContactsPatchSchema, createContactsSchema } from "../validation/contacts.js";
 // import { ctrlWrapper } from "../utilits/ctrlWrapper.js";
 // import { validateObjectId } from "../middlewares/validateObjectId.js";
 
@@ -11,6 +11,6 @@ const router = Router();
 router.get('/',(getContactsController));
 router.get('/:contactId', isValidId, (getContactsIdController));
 router.post('/', validateBody(createContactsSchema), createContactController);
-router.patch('/:contactId', isValidId, validateBody(createContactsSchema),   patchContactController);
+router.patch('/:contactId', isValidId, validateBody(createContactsPatchSchema),   patchContactController);
 router.delete('/:contactId', isValidId,  deleteContactController);
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -7,3 +7,11 @@ export const createContactsSchema = Joi.object({
     isFavourite: Joi.boolean(),
     contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal').required(),
 });
+
+export const createContactsPatchSchema = Joi.object({
+    name: Joi.string().min(3).max(20),
+    phoneNumber: Joi.string().min(3).max(20),
+    email: Joi.string().min(3).max(20),
+    isFavourite: Joi.boolean(),
+    contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
+});
