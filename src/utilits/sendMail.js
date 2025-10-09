5f60aa01d2c99c45779aa2dbc9979130aa9328c7
Yevhen Bozadzhi <ebozadji13@gmail.com>
2025-10-09 20:38:41 +0300

    fixed sendMail

--- src/utilits/sendMail.js
+++ src/utilits/sendMail.js
@@ -12,5 +12,5 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async (options) => {
-    return await transporter.sendEmail(options);
+    return await transporter.sendMail(options);
 }
